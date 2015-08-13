2134879825bbb3d43be798666e1c267f622c9c4a
Stephane Lorcy <stephane.lorcy@gmail.com>
2015-08-13 16:41:37 +0200

    Ajout synchro animaux

--- www/js/app.js
+++ www/js/app.js
@@ -24,9 +24,13 @@ angular.module('starter', ['ionic','synchro'])
 angular.module('synchro', [])
 
 .factory('synchroConfiguration', function() {
+
+  var ROOT_DB = "http://localhost:5984";
+
   return {
     userId : "slo",
-    usersUrl : "http://localhost:5984/users"
+    usersUrl : ROOT_DB+"/users",
+    animauxUrl : ROOT_DB+"/animaux"
   }
 })
 
@@ -58,7 +62,132 @@ angular.module('synchro', [])
   }
 }])
 
-.factory('synchroManager', ["$log", "$q", 'synchroConfiguration', 'db', function($log, $q, sc, db) {
+.factory('synchroAnimaux', ["$log", "$q", 'synchroConfiguration', 'db', function($log, $q, sc, db) {
+  // Service de synchronisation des animaux ...
+  var animauxDB = db.get('animaux');
+  var handler;
+  var animauxByElevage = {};
+  var animaux_q = $q.defer();
+
+  function updateAnimal(animaux, animal) {
+    for(var i = 0; i<animaux.length; i++) {
+      if(animaux[i].id==animal.id) {
+        if(animal.deleted) {
+          animaux.splice(i,1);
+        } else {
+          animaux[i]=animal;
+        }
+        return;
+      }
+    }
+    animaux.push(animal);
+  }
+
+  function pushAnimal(elevage, animal) {
+    var animaux = animauxByElevage[elevage]?animauxByElevage[elevage]:[];
+    updateAnimal(animaux, animal);
+    animauxByElevage[elevage]=animaux;
+  }
+
+  var scopes = [];
+
+  var addScope = function(scope) {
+    $log.log("Adding a new scope for animaux entities ...");
+    scopes.push(scope);
+  }
+
+  var removeScope = function(scope) {
+    $log.log("Removing a scope for animaux entities ...");
+    for(var i = 0; i<scopes.length; i++) {
+      if(scopes[i]===scope) {
+        scopes.splice(i,1);
+      }
+    }
+  }
+
+  var applyScope = function() {
+    scopes.forEach(function(scope) {
+      $log.log("Applying scope");
+      scope.$apply();
+    });
+  }
+
+  var stopSync = function() {
+    if(handler) {
+      $log.log("Stopping sync animaux entities")
+      handler.cancel();
+      handler=null;
+    }
+  }
+
+  var updateAnimaux = function(docs) {
+      $log.log("updateAnimaux");
+      docs.forEach(function(doc) {
+        pushAnimal(doc.idElevage, {id:doc._id, race:doc.race, name:doc.name, deleted:doc._deleted});        
+      });
+      applyScope();
+  }
+
+  //First initialization
+  animauxDB.allDocs({
+    include_docs: true
+  }).then(function (result) {
+    $log.log(JSON.stringify(result));
+    result.rows&&updateAnimaux(result.rows.map(function(row) { return row.doc }));
+    return animaux_q.resolve(animauxByElevage);
+  }).catch(function (err) {
+    $log.log(err);
+  });
+
+  return {
+    changeSync: function(idElevages) {
+      stopSync();
+      $log.log("Starting sync animaux entities "+JSON.stringify(idElevages));
+      handler = animauxDB.replicate.from(sc.animauxUrl, {
+        live:true,
+        retry:true,
+        filter: 'custom/byElevages',
+        query_params: {elevagesIds: idElevages}
+      })
+      .on('change', function (change) {
+        $log.log("change :"+JSON.stringify(change.docs));
+        updateAnimaux(change.docs);
+        applyScope();
+      })
+      .on('paused', function (info) {
+        // replication was paused, usually because of a lost connection
+        $log.log("paused :"+JSON.stringify(info));
+      })
+      .on('active', function (info) {
+        // replication was resumed
+        $log.log("active :"+JSON.stringify(info));
+      })
+      .on('error', function (err) {
+        // totally unhandled error (shouldn't happen)
+        $log.log("error :"+JSON.stringify(err));
+      });
+
+    },
+
+    forceSync: function(animal) {
+
+    },
+
+    listenAnimaux: function(scope) {
+      if(scope) {
+        addScope(scope);
+      }
+      return animaux_q.promise;
+    },
+
+    unlistenAnimaux: function(scope) {
+      removeScope(scope);
+    }
+
+  };
+}])
+
+.factory('synchroManager', ["$log", "$q", 'synchroConfiguration', 'db', 'synchroAnimaux', function($log, $q, sc, db, sa) {
   // This simple service control the synchronization for a specific user
   var userDb = db.get('user');
   var handler;
@@ -102,6 +231,9 @@ angular.module('synchro', [])
     userDoc.tournees.forEach(function(t) {
       addElevage(t.id, t.date, t.name);
     });
+    //var idElevages = elevages.map(function(e) { return e.id });
+    //$log.log(JSON.stringify(idElevages));
+    sa.changeSync(elevages.map(function(e) { return e.id }));
   }
 
   var scopes = [];
@@ -173,17 +305,59 @@ angular.module('synchro', [])
         handler.cancel();
       }
     }
-  }
+  };
 }])
 
-.controller('elevagesCtrl', ['$log', '$scope', 'synchroManager', function($log, $scope, sm) {
+.controller('elevagesCtrl', ['$log', '$scope', 'synchroManager', '$ionicModal','synchroAnimaux', function($log, $scope, sm, im, sa) {
+
+  // On s'abonne aux modifications des elevages suite à syncrho
+
   sm.listenElevages($scope).then(function(elevages) {
     $scope.elevages = elevages;
   });
+
+  sa.listenAnimaux($scope).then(function(animauxByElevage) {
+    $scope.animauxByElevage = animauxByElevage;
+  });
+
+  // Dans le cas ou le scope disparait : on se désabonne ...
+
   $scope.$on("$destroy", function handler() {
     sm.unlistenElevages($scope);
+    sa.unlistenElevages($scope);
   });
+
   $scope.log = function() {
-    $log.log(JSON.stringify($scope.elevages));
+    $log.log("Elevages :" + JSON.stringify($scope.elevages));
+  };
+
+  // Voir l'elevage
+  $scope.showElevage = function(elevage) {
+    $scope.oldElevageName = elevage.name;
+    $scope.elevage = elevage;
+    $scope.elevageModal.show();
   }
+
+  // Retour à la liste
+  $scope.hideElevage = function() {
+    $scope.elevageModal.hide();
+  }
+
+  // Annulation
+  $scope.cancelElevage = function() {
+    $scope.elevage.name = $scope.oldElevageName;
+    $scope.hideElevage();
+  }
+
+  // Gestion du detail : on utilise la fonction modale de ionic.
+
+  // Create and load the Modal
+  im.fromTemplateUrl('show-elevage.html', function(modal) {
+    $scope.elevageModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+
 }])
